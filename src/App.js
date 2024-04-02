77bbc3dc8a101f844a2f8d083f972e45c89177f3
Sarah Barber <sarahbarberuk@gmail.com>
2024-04-02 12:26:39 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Suspense } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const query = `
+{
+  productCollection {
+    items {
+			description
+      imageUrl {   
+        url
+      }
+    }
+  }
 }
+`;
+
+const response = await fetch(
+  `https://graphql.contentful.com/content/v1/spaces/<space_id>/`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // Authenticate the request
+      Authorization: "Bearer <API_TOKEN>",
+    },
+    //send the GraphQL query
+    body: JSON.stringify({ query }),
+  }
+);
+
+const data = await response.json();
+
+if (!response.ok) {
+  throw new Error("error");
+}
+
+const App = () => {
+  return (
+    <Suspense fallback={<p>Fetching products...</p>}>
+      <ProductCollection />
+    </Suspense>
+  );
+};
+
+const ProductCollection = () => {
+  const products = data.data.productCollection.items;
+  const listItems = products.map((product) => (
+    <li>
+      <img src={product.imageUrl.url} width="200" height="200" />
+      <p>{product.description}</p>
+    </li>
+  ));
+  return <ul>{listItems}</ul>;
+};
 
 export default App;
